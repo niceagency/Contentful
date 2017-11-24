2d2067114d8d72a48d2b322844a5d58147ce7f3d
Sam Woolf <sam.woolf@nice.agency>
2017-11-24 10:54:14 +0000

    included header in response

--- Contentful/Contentful/Contentful.swift
+++ Contentful/Contentful/Contentful.swift
@@ -88,12 +88,16 @@ public struct ObjectEncoding {
 
 public struct Publishing {
     
-    public static func preparePublishRequest<T: Writeable> (forEntry entry: T, toSpace spaceId: String ) -> String {
-        return  "/spaces/\(spaceId)/entries/\(entry.contentful_id)/published"
+    public static func preparePublishRequest<T: Writeable> (forEntry entry: T, toSpace spaceId: String ) -> (endpoint:String, header:  (String,String))  {
+        let endpoint =   "/spaces/\(spaceId)/entries/\(entry.contentful_id)/published"
+        let header = ("X-Contentful-Version","\(entry.contentful_version)")
+        return (endpoint: endpoint, header: header)
     }
     
-    public static func preparePublishRequest (forEntryID entryID: String, toSpace spaceId: String ) -> String {
-        return  "/spaces/\(spaceId)/entries/\(entryID)/published"
+    public static func preparePublishRequest (forEntryID entryID: String, entryVersion version: String,  toSpace spaceId: String ) -> (endpoint:String, header:  (String,String)) {
+        let endpoint =  "/spaces/\(spaceId)/entries/\(entryID)/published"
+        let header = ("X-Contentful-Version","\(version)")
+        return (endpoint: endpoint, header: header)
     }
 }
 
